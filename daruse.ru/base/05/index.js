a6f7510a8e713492aec75882e47f78a7ab6f739f
alexmacbook <alex_yasinsky@mail.ru>
2022-02-10 21:47:11 +0300

    daruse.ru/base/05 done

--- daruse.ru/base/05/index.js
+++ daruse.ru/base/05/index.js
@@ -6,6 +6,10 @@ const str = rls.question('Enter text \n');
 
 let arr = [];
 
-arr.push(str.match(/[!\?\.] /gi));
+const firstWord = (str.match(/^[а-я]*/)[0]);
+let otherWords = str.match(/[!\?\.]\s[а-я]*/gi);
+otherWords = otherWords.map(word => word.slice(2));
+
+arr = arr.concat(firstWord, otherWords);
 
 console.log(arr);
